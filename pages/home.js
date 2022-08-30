8a8a5745aada3247fd1455028f8d375a0418e0aa
Geeky Coder <toyibe25@gmail.com>
2022-08-31 00:26:12 +0100

    initial commit

---
+++ pages/home.js
@@ -0,0 +1,25 @@
+import Head from '../src/components/Meta';
+import Header from '../src/components/Header';
+import Tests from '../src/components/Tests';
+import ChartContainer from '../src/components/Chartconatiner';
+
+export const getStaticProps = async () => {
+	const res = await fetch('https://mabifus.vercel.app/api/test');
+	const data = await res.json();
+	return {
+		props: { testsdata: data },
+	};
+};
+
+function Home({ testsdata }) {
+	return (
+		<section className="home">
+			<Head title="Homepage" content="Homepage" />
+			<Header />
+			{testsdata && <Tests tests={testsdata} />}
+			<ChartContainer />
+		</section>
+	);
+}
+
+export default Home;
