c7cb96deb513ecbc36958e1f76d9e841364c6893
Geeky Coder <96173120+GeekyCoder25@users.noreply.github.com>
2022-09-21 07:55:40 +0100

    Delete src/components/doctorslist directory

--- src/components/doctorslist/[doctor].js
+++
@@ -1,7 +0,0 @@
-import data from '../../data/doctorsdb.json';
-export default function handler(req, res) {
-	const selectUserDoctor = data.doctors.find(
-		detail => detail.name === req.query.doctor
-	);
-	res.status(200).json(selectUserDoctor);
-}
